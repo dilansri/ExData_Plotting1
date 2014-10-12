908cbe8f51ee97fb8b35651e6adea07685a6912b
Dilan Srilal <dilansri@gmail.com>
2014-10-12 17:58:36 +0530

    Plot 1 changed

--- plot1.R
+++ plot1.R
@@ -1,7 +1,13 @@
-#reading data 
-data <- read.table("household_power_consumption.txt",sep=";",header=FALSE,skip = 66637, nrow = 2880)
-#replace ? with NAs
-data[data == "?"] <- "NA"
+#Reading the data file
+house_data <- read.table("household_power_consumption.txt",na.strings="?",sep=";",header=TRUE)
 png("plot1.png", width = 480, height = 480)
-hist(data$V3, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
+
+#Subsetting data from 2007-02-01 to 2007-02-02
+
+house_data$Date <- as.Date(house_data$Date,format="%d/%m/%Y")
+
+house_data_subset <- subset(house_data,Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+
+# plotting to graphic device
+hist(house_data_subset$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
 dev.off()
